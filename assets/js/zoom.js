39deae4d3276b85d36d6014898a402dfab4fd858
malka88 <malka_88@outlook.com>
2022-07-12 14:53:39 +0700

    Update all

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,16 +1,9 @@
-var zoomFigure1 = document.getElementById('zoomFigure1');
-var zoomFigure2 = document.getElementById('zoomFigure2');
-var zoomFigure3 = document.getElementById('zoomFigure3');
-var zoomImage = document.getElementById('zoomImage');
+var mainImage = document.getElementsByClassName('product__main-image');
+var zoomImages = document.getElementsByClassName('product__additional-image');
 
-zoomFigure1.addEventListener("mouseover", function(){
-    zoomImage.src = "assets/img/Слой 48.png";
-})
-
-zoomFigure2.addEventListener("mouseover", function(){
-    zoomImage.src = "assets/img/Слой 49.png";
-})
-
-zoomFigure3.addEventListener("mouseover", function(){
-    zoomImage.src = "assets/img/Слой 50.png";
-})
+for(var i = 0; i < zoomImages.length; i++){
+    zoomImages[i].addEventListener("mouseover", function(event) {
+        var target = event.target;
+        mainImage[0].src = target.src;
+    });
+};
